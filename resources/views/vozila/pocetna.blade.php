11ff8f6679fc801c3cef9629dde242e0d151f1c4
mkrivic83 <krivic.milan@gmail.com>
2026-02-04 18:00:48 +0100

    zadnja prilika

--- resources/views/vozila/pocetna.blade.php
+++ resources/views/vozila/pocetna.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="flex justify-between items-center mb-6">
-        <p>Dobro došli na deployanu aplikaciju još jednom i sad zadnji i zadnja promjena</p>
+        <p>Dobro došli na deployanu aplikaciju još jednom i sad zadnji i zadnja promjena još jedna</p>
     </div>
     <div class="text-sm text-gray-500">
         Environment: {{ app()->environment() }} |
